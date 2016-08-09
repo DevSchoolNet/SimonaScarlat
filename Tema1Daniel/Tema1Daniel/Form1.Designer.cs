a34df6fc2ac80bdcc76a9dae1b0d59978e38e634
Scarlat Simona <simonascarlat6@gmail.com>
2016-08-09 08:07:30 +0000

    C# 6.0

---
+++ Tema1Daniel/Tema1Daniel/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Tema1Daniel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.idApartment_textbox = new System.Windows.Forms.TextBox();
+            this.numberOfRooms_textbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addApartment_button = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.floor_textBox = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.idApartmentColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.numberOfRoomsColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // idApartment_textbox
+            // 
+            this.idApartment_textbox.Location = new System.Drawing.Point(435, 108);
+            this.idApartment_textbox.Name = "idApartment_textbox";
+            this.idApartment_textbox.Size = new System.Drawing.Size(100, 20);
+            this.idApartment_textbox.TabIndex = 1;
+            // 
+            // numberOfRooms_textbox
+            // 
+            this.numberOfRooms_textbox.Location = new System.Drawing.Point(434, 186);
+            this.numberOfRooms_textbox.Name = "numberOfRooms_textbox";
+            this.numberOfRooms_textbox.Size = new System.Drawing.Size(100, 20);
+            this.numberOfRooms_textbox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(433, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(432, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number of rooms";
+            // 
+            // addApartment_button
+            // 
+            this.addApartment_button.Location = new System.Drawing.Point(434, 229);
+            this.addApartment_button.Name = "addApartment_button";
+            this.addApartment_button.Size = new System.Drawing.Size(84, 26);
+            this.addApartment_button.TabIndex = 5;
+            this.addApartment_button.Text = "Add apartment";
+            this.addApartment_button.UseVisualStyleBackColor = true;
+            this.addApartment_button.Click += new System.EventHandler(this.addApartment_button_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(848, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(433, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Floor";
+            // 
+            // floor_textBox
+            // 
+            this.floor_textBox.Location = new System.Drawing.Point(435, 147);
+            this.floor_textBox.Name = "floor_textBox";
+            this.floor_textBox.Size = new System.Drawing.Size(100, 20);
+            this.floor_textBox.TabIndex = 7;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.idApartmentColumn,
+            this.numberOfRoomsColumn});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(42, 92);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(235, 163);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // idApartmentColumn
+            // 
+            this.idApartmentColumn.Text = "Id";
+            this.idApartmentColumn.Width = 98;
+            // 
+            // numberOfRoomsColumn
+            // 
+            this.numberOfRoomsColumn.Text = "Number of rooms";
+            this.numberOfRoomsColumn.Width = 129;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 343);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.floor_textBox);
+            this.Controls.Add(this.addApartment_button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numberOfRooms_textbox);
+            this.Controls.Add(this.idApartment_textbox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox idApartment_textbox;
+        private System.Windows.Forms.TextBox numberOfRooms_textbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button addApartment_button;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox floor_textBox;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader idApartmentColumn;
+        private System.Windows.Forms.ColumnHeader numberOfRoomsColumn;
+    }
+}
+
---
+++ Tema1Daniel/Tema1Daniel/Form2.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Tema1Daniel
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idRoom_textBox = new System.Windows.Forms.TextBox();
+            this.width_textBox = new System.Windows.Forms.TextBox();
+            this.height_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.orientation_comboBox = new System.Windows.Forms.ComboBox();
+            this.addRoom_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // idRoom_textBox
+            // 
+            this.idRoom_textBox.Location = new System.Drawing.Point(75, 89);
+            this.idRoom_textBox.Name = "idRoom_textBox";
+            this.idRoom_textBox.Size = new System.Drawing.Size(100, 20);
+            this.idRoom_textBox.TabIndex = 0;
+            // 
+            // width_textBox
+            // 
+            this.width_textBox.Location = new System.Drawing.Point(75, 125);
+            this.width_textBox.Name = "width_textBox";
+            this.width_textBox.Size = new System.Drawing.Size(100, 20);
+            this.width_textBox.TabIndex = 1;
+            // 
+            // height_textBox
+            // 
+            this.height_textBox.Location = new System.Drawing.Point(75, 161);
+            this.height_textBox.Name = "height_textBox";
+            this.height_textBox.Size = new System.Drawing.Size(100, 20);
+            this.height_textBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Height";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Orientation";
+            // 
+            // orientation_comboBox
+            // 
+            this.orientation_comboBox.FormattingEnabled = true;
+            this.orientation_comboBox.Items.AddRange(new object[] {
+            "N",
+            "S",
+            "E",
+            "V"});
+            this.orientation_comboBox.Location = new System.Drawing.Point(75, 196);
+            this.orientation_comboBox.Name = "orientation_comboBox";
+            this.orientation_comboBox.Size = new System.Drawing.Size(121, 21);
+            this.orientation_comboBox.TabIndex = 7;
+            // 
+            // addRoom_button
+            // 
+            this.addRoom_button.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.addRoom_button.Location = new System.Drawing.Point(75, 256);
+            this.addRoom_button.Name = "addRoom_button";
+            this.addRoom_button.Size = new System.Drawing.Size(75, 23);
+            this.addRoom_button.TabIndex = 8;
+            this.addRoom_button.Text = "Add room";
+            this.addRoom_button.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 337);
+            this.Controls.Add(this.addRoom_button);
+            this.Controls.Add(this.orientation_comboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.height_textBox);
+            this.Controls.Add(this.width_textBox);
+            this.Controls.Add(this.idRoom_textBox);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.TextBox idRoom_textBox;
+        public System.Windows.Forms.TextBox width_textBox;
+        public System.Windows.Forms.TextBox height_textBox;
+        public System.Windows.Forms.ComboBox orientation_comboBox;
+        public System.Windows.Forms.Button addRoom_button;
+    }
+}
