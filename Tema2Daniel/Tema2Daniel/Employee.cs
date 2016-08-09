3060d10db5599c93cabd431ebd2cf2222e6df1fb
Scarlat Simona <simonascarlat6@gmail.com>
2016-08-09 09:49:36 +0000

    covariance

---
+++ Tema2Daniel/Tema2Daniel/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema2Daniel
+{
+    class Employee:Person
+    {
+
+        public string employeeId;
+
+        public Employee(string employeeId, string name, string phone, string email) : base(name, phone, email)
+        {
+            this.employeeId = employeeId;
+        }
+      
+
+        public override string ToString()
+        {
+            return base.ToString() + "EmployeeId: " + this.employeeId;
+        }
+    }
+}
